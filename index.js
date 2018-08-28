12dc94027cbd5c50169d6bb0847006af70842669
Ariel Matsui-LeRoy <arielmatsuileroy@gmail.com>
2018-08-28 15:51:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
+  return object
 }
 
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object)
   delete newObject[key];
+  return newObject;
 }
