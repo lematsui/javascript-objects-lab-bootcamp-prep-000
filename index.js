abf1e99f2047120430ccba523a99495da324c538
Ariel Matsui-LeRoy <arielmatsuileroy@gmail.com>
2018-08-28 15:31:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign(object, {key: value} ) = newObject;
+  Object.assign(object, {key: value}) = newObject;
   return newObject;
 }
